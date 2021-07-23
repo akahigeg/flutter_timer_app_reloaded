5579380b8b0c00aba93bb6905cfe43e47054fedf
akahige <akahigeg@gmail.com>
2021-07-23 18:20:29 +0900

    タイマー切り替えの仮実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:vector_math/vector_math.dart' as vector_math;
 
 void main() {
@@ -50,12 +53,38 @@ class MyApp extends ConsumerWidget {
 // }
 
 class Timer {
-  Timer({required this.min, required this.sec, required this.msec});
+  final int timerId;
 
-  final int min;
-  final int sec;
-  final int msec;
+  int initialMin = 0;
+  int initialSec = 0;
+
+  int currentMin = 0;
+  int currentSec = 0;
+  int currentMsec = 0;
+
+  Timer({required this.timerId}) {
+    restore();
+  }
+
+  void restore() async {
+    var prefs = await SharedPreferences.getInstance();
+    var timer = prefs.getString(this.timerId.toString()) ?? "03:00:00";
+    var numbers = timer.toString().split(":");
+
+    this.initialMin = int.parse(numbers[0]);
+    this.initialSec = int.parse(numbers[1]);
+
+    this.currentMin = this.initialMin;
+    this.currentSec = this.initialSec;
+    this.currentMsec = 0;
+
+    // update();
+    // notifyListeners();
+  }
 }
+
+final timerIdProvider = StateProvider((ref) => 1);
+final timerProvider = StateProvider((ref) => Timer(timerId: 1));
 
 class FlutterTimer extends StatelessWidget {
   FlutterTimer({Key? key}) : super(key: key);
@@ -73,28 +102,40 @@ class FlutterTimer extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text("Flutter Timer"), actions: <Widget>[]),
         body: IconTheme(
-            data: IconThemeData(color: Colors.black.withOpacity(0.8)),
-            child: Stack(alignment: AlignmentDirectional.center, children: <Widget>[
+          data: IconThemeData(color: Colors.black.withOpacity(0.8)),
+          child: Consumer(builder: (context, ref, child) {
+            final timerId = ref.watch(timerIdProvider);
+            return Stack(alignment: AlignmentDirectional.center, children: <Widget>[
               PageView.builder(
                   physics: AlwaysScrollableScrollPhysics(),
                   controller: _pageController,
                   itemBuilder: (BuildContext context, int index) {
+                    // timerProvider = StateProvider((ref) => Timer(timerId: index % _pages.length + 1));
+                    // print("index: $index");
+                    print("timerId: ${index % _pages.length + 1}");
                     return Column(children: [_pages[index % _pages.length]]);
                   },
                   onPageChanged: (int page) {
+                    timerId.state = page % _pages.length + 1;
+
                     // TODO: アクティブなタイマーの切り替え
+                    // timerProvider = StateProvider((ref) => Timer(timerId: page % _pages.length + 1));
+                    // print("page: $page");
+                    // print("page % _pages.length + 1: ${page % _pages.length + 1}");
 
                     // TODO: ドットインジケーターのポジションの更新
                   }),
               // TODO: ドットインジケーターの表示
-            ])));
+            ]);
+          }),
+        ));
   }
 }
 
 class TimerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (context, timer, child) {
+    return Consumer(builder: (context, ref, child) {
       return Container(
           child: Stack(
         alignment: AlignmentDirectional.center,
@@ -114,8 +155,6 @@ class TimerWidget extends StatelessWidget {
   }
 }
 
-final timerProvider = StateProvider((ref) => Timer(min: 1, sec: 2, msec: 3));
-
 class Display extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -127,15 +166,15 @@ class Display extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Text(
-            "${timer.min.toString().padLeft(2, '0')}:",
+            "${timer.currentMin.toString().padLeft(2, '0')}:",
             style: Theme.of(context).textTheme.headline2,
           ),
           Text(
-            "${timer.sec.toString().padLeft(2, '0')}:",
+            "${timer.currentSec.toString().padLeft(2, '0')}:",
             style: Theme.of(context).textTheme.headline2,
           ),
           Text(
-            "${timer.msec.toString().padLeft(2, '0')}",
+            "${timer.currentMsec.toString().padLeft(2, '0')}",
             style: Theme.of(context).textTheme.headline3,
           ),
         ],
@@ -148,9 +187,11 @@ class TimerName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
+      final timerId = ref.watch(timerIdProvider).state;
+
       return Container(
         alignment: AlignmentDirectional.center,
-        child: Text("TIMER-XX", style: TextStyle(color: Colors.white)),
+        child: Text("TIMER-${timerId.toString().padLeft(2, '0')}", style: TextStyle(color: Colors.white)),
       );
     });
   }
